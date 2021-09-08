c647990ba1cd17d3f8e83ec7c6b83c951d37a4f1
MassterMinde <AlexanderAnikin2002@yandex.ru>
2021-09-08 19:29:35 +0300

    Initial commit

---
+++ src/com/company/Kniga.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Kniga {
+    String book_name="Война и мир";
+    String book_cost="3 руб.";
+    String book_age="15 л.";
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        Kniga kniga = new Kniga();
+        System.out.println(kniga.book_name);
+        System.out.println(kniga.book_cost);
+        System.out.println(kniga.book_age);
+    }
+}
